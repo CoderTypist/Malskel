7b7f54bc9196315507ae562c62fc6b5723c614fc
CoderTypist <cjbargraser@gmail.com>
2020-10-05 01:14:33 -0500

    Structs are passed to threads and process death is detected.

--- malskel.c
+++ malskel.c
@@ -4,8 +4,7 @@
 // - set bDebug to false
 
 // To make static binary analysis more difficult consider:
-// - removing all print statements
-// - getting rid of the usage function
+// - removing all DEBUG print statements
 
 #include <pthread.h>
 #include <stdio.h>
@@ -16,159 +15,174 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define LAUNCH  1
-#define MONITOR 2
-#define PAYLOAD 3
+// lets monitor() know how to respond to a dead process
+#define MONITOR 1
+#define PAYLOAD 2
+
+// monitor() sleep time within spinlock
+#define NAP_TIME 0.25
 
 #define DEBUG if( true == bDebug )
 bool bDebug = true;
 
+typedef struct monitor_args {
+    int iMode;
+    pid_t pid_monitor; // pid to monitor
+} monitor_args;
+
+typedef struct payload_args {
+    int argc;
+    char **argv;
+} payload_args;
+
 void* monitor(void *arg);
 void* payload(void *arg);
-void usage();
+payload_args* get_payload_args(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
 
-    // a mode must be provided    
-    if( argc <= 1 ) {
-
-        DEBUG { 
-            printf("Not enough arguments\n\n");
-            usage();
-        }
-
-        exit(-1);
-    }
-
-    // get the mode
-    int iMode;
-
-    if( 0 == strcmp(argv[1], "-l") || 0 == strcmp(argv[1], "--launch") ) {
-        iMode = LAUNCH;
-    }
-
-    else if( 0 == strcmp(argv[1], "-m") || 0 == strcmp(argv[1], "--monitor") ) {
-        iMode = MONITOR;
-    }
-
-    else if( 0 == strcmp(argv[1], "-p") || 0 == strcmp(argv[1], "--payload") ) {
-        iMode = PAYLOAD;
-    }
-
-    else {
-
-        DEBUG {
-            printf("Invalid mode: %s\n\n", argv[1]);
-            usage();
-        }
-
-        exit(-1);
-    }
-    
     pthread_t tid_payload;
     pthread_t tid_monitor;
     pid_t pid;
 
-    // initial deployment
-    if( LAUNCH == iMode ) {
+    DEBUG fprintf(stderr, "Mode: LAUNCH\n");
 
-        DEBUG fprintf(stderr, "Mode: LAUNCH\n");
+    pid = fork();
 
-        pid = fork();
-
-        if( pid < 0 ) {
-            
-            DEBUG fprintf(stderr, "\nFork failed\n");
-            exit(-1);
-        }
-
-        // execute payload and monitor the process monitoring this process
-        else if( 0 == pid ) {
-            
-            iMode = PAYLOAD;
-
-            // execute the payload
-            pthread_create(&tid_payload, (void*)NULL, payload, (void*)NULL);
-            DEBUG fprintf(stderr, "    Thread (LAUNCH: payload->payload): tid = %ld from pid = %d\n", tid_payload, getpid());
-
-            // monitor the parent
-            pid_t pid_parent = getppid();
-            pthread_create(&tid_monitor, (void*)NULL, monitor, &pid_parent);
-            DEBUG fprintf(stderr, "    Thread (LAUNCH: payload->monitor): tid = %ld from pid = %d\n", tid_monitor, getpid());
-
-            pthread_join(tid_payload, (void*)NULL);
-            pthread_join(tid_monitor, (void*)NULL);
-        }
-
-        // monitor the process executing the payload
-        else {
-            
-            iMode = MONITOR;
-
-            // monitor the child (payload)
-            pthread_create(&tid_monitor, (void*)NULL, monitor, &pid);
-            DEBUG fprintf(stderr, "    Thread (LAUNCH: monitor->monitor): tid = %ld from pid = %d\n", tid_monitor, getpid());
-            pthread_join(tid_monitor, (void*)NULL);
-
-            while( -1 != wait(NULL) );
-        }
-    }
-
-    // the invoking process should be the monitor from a process in PAYLOAD mode
-    else if ( MONITOR == iMode && argc >= 3 ) {
-
-        DEBUG fprintf(stderr, "Mode: MONITOR\n");
-        pid_t pid_monitor = (pid_t)atoi(argv[2]);
-        monitor(&pid_monitor);
-    }
-
-    // the invoking process should be the monitor from a process in MONITOR mode
-    else if ( PAYLOAD == iMode && argc >= 3 ) {
+    if( pid < 0 ) {
         
-        DEBUG fprintf(stderr, "Mode: PAYLOAD\n"); 
+        DEBUG fprintf(stderr, "\nFork failed\n");
+        exit(-1);
+    }
+
+    // child: execute payload and monitor the parent
+    else if( 0 == pid ) {
 
         // execute the payload
-        pthread_create(&tid_payload, (void*)NULL, payload, (void*)NULL);
-        DEBUG fprintf(stderr, "    Thread (PAYLOAD: payload->payload): tid = %ld from pid = %d\n", tid_payload, getpid());
+        pthread_create(&tid_payload, (void*)NULL, payload, (void*)get_payload_args(argc, argv));
+        DEBUG fprintf(stderr, "    Thread (payload->payload): tid = %ld from pid = %d\n", tid_payload, getpid());
 
-        // monitor the invoking process
-        pid_t pid_monitor = (pid_t)atoi(argv[2]);
-        pthread_create(&tid_monitor, (void*)NULL, monitor, &pid_monitor);
-        DEBUG fprintf(stderr, "    Thread (PAYLOAD: payload->monitor): tid = %ld from pid = %d\n", tid_monitor, getpid());
+        // monitor the parent
+        pid_t pid_parent = getppid();
+        monitor_args ma = { PAYLOAD, pid_parent };
+        pthread_create(&tid_monitor, (void*)NULL, monitor, &ma);
+        DEBUG fprintf(stderr, "    Thread (payload->monitor): tid = %ld from pid = %d\n", tid_monitor, getpid());
 
         pthread_join(tid_payload, (void*)NULL);
         pthread_join(tid_monitor, (void*)NULL);
-
-        payload((void*)NULL);
     }
 
+    // parent: monitor the the child (payload)
     else {
 
-        DEBUG {
-            printf("Invalid mode and/or arguments\n\n");
-            usage();
-        }
+        monitor_args ma = { MONITOR, pid };
+        pthread_create(&tid_monitor, (void*)NULL, monitor, &ma);
+        DEBUG fprintf(stderr, "    Thread (monitor->monitor): tid = %ld from pid = %d\n", tid_monitor, getpid());
+        pthread_join(tid_monitor, (void*)NULL);
 
-        exit(-1);
+        while( -1 != wait(NULL) );
     }
 }
 
 void* monitor(void *arg) {
     
-    pid_t pid_monitor = *((pid_t*)arg);
-    DEBUG fprintf(stderr, "Monitor: pid = %d -> pid = %d\n", getpid(), pid_monitor);
+    monitor_args ma = *((monitor_args*)arg);
+    pid_t pid_monitor = ma.pid_monitor;
+    DEBUG fprintf(stderr, "Monitor: pid = %d -> pid = %d\n", getpid(), ma.pid_monitor);
+
+    while ( true ) {
+
+        // free the PCB if the process is dead
+        waitpid(pid_monitor, NULL, WNOHANG);
+
+        // check to see if the process is dead
+        if( 0 != kill(pid_monitor, 0) ) {
+
+            // if the current process is a PAYLAOD
+            if( MONITOR == ma.iMode ) {
+                fprintf(stderr, "Monitor: payload process was killed\n");
+
+                // WORKS! Dead process is detected
+                // payload_args
+                // fork -> payload thread AND monitor thread
+            }
+
+            // if the current process is a MONITOR
+            else if ( PAYLOAD == ma.iMode ) {
+                fprintf(stderr, "Payload: monitor process was killed\n");
+
+                // WORKS! Dead process is detected
+                // monitor_args
+                // fork -> call monitor (no need for a thread)
+            }
+
+            // unexpected behavior
+            else {
+                DEBUG fprintf(stderr, "Unexpected beavior\n");
+                exit(-1);
+            }
+        }
+
+        // adjust sleep time as needed
+        sleep(NAP_TIME);
+    }
+
     return NULL;
 }
 
 void* payload(void *arg) {
 
+    // process arg
+    payload_args pa = *((payload_args*)arg);
+    int argc = pa.argc;
+    char** argv = pa.argv;
+
+    // -- remove later --
+    // WORKS! Arguments are received
+    // fprintf(stderr, "Payload: argc = %d\n", argc);
+    // fprintf(stderr, "Payload: argv[0] = %s\n", argv[0]);
+
     DEBUG fprintf(stderr, "Payload: pid = %d\n", getpid());
+
+    sleep(100);
+
     return NULL;
 }
 
-void usage() {
+payload_args* get_payload_args(int argc, char *argv[]) {
 
-    printf("./malskel -l/--launch <payload_args>\n");
-    printf("./malskel -m/--monitor <pid_monitor> <payload_args>\n");
-    printf("./malskel -p/--payload <pid_monitor> <payload_args>\n");
+    payload_args *ppa = (payload_args*)malloc(sizeof(payload_args));
+
+    if( NULL == ppa ) {
+        DEBUG fprintf(stderr, "Error: Malloc for ppa failed\n");
+        exit(-1);
+    }
+
+    // if there are no payload arguments
+    if( 1 == argc ) {
+        ppa->argc = 0;
+        ppa->argv = NULL;
+        return ppa;
+    }
+
+    // char** that will point to payload arguments in argv
+    fprintf(stderr, "argc: %d\n", argc);
+    char** p_argv = (char**)malloc( (argc-1) * sizeof(char*) );
+
+    if( NULL == p_argv ) {
+        DEBUG fprintf(stderr, "Error: Malloc for p_argv failed\n");
+        exit(-1);
+    }
+
+    // make the char** point to the arguments in argv
+    int i;
+    for( i = 0 ; i < argc-1 ; i++ ) {
+        p_argv[i] = argv[i+1];
+        // fprintf(stderr, "p_argv[%d] = %s\n", i, p_argv[i]);
+    }
+
+    ppa->argc = argc-1;
+    ppa->argv = p_argv;
+    
+    return ppa;
 }
